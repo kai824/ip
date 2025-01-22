30f547e7d1f0701209b1a117b194b71e90f882d1
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-01-22 22:37:52 +0800

    Implement Add, List feature

--- src/main/java/Adam.java
+++ src/main/java/Adam.java
@@ -1,12 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Adam {
+    private static ArrayList<String> history;
+
     private static void printSeparatingLine() {
         System.out.println("    ____________________________________________________________");
     }
 
     private static void outputText(String text) {
         System.out.println("     " + text);
+    }
+
+    private static void listAll() {
+        for (int i = 0; i < Adam.history.size(); i++) {
+            outputText(String.format("%d. %s", i + 1, Adam.history.get(i)));
+        }
+    }
+
+    private static void addNew(String text) {
+        Adam.history.add(text);
+        outputText("added: " + text);
     }
 
     public static void main(String[] args) {
@@ -17,17 +31,21 @@ public class Adam {
         printSeparatingLine();
 
         Scanner sc = new Scanner(System.in);
-        boolean exited = false;
+        history = new ArrayList<String>();
 
-        while(!exited) {
+        while(true) {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
-                exited = true;
-            } else {
-                printSeparatingLine();
-                outputText(userInput);
-                printSeparatingLine();
+                break;
             }
+            
+            printSeparatingLine();
+            if (userInput.equals("list")){
+                listAll();
+            } else {
+                addNew(userInput);
+            }
+            printSeparatingLine();
         }
 
         printSeparatingLine();
