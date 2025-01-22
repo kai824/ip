5f83fffdf27d14ea228dbd5c4eda19fe2d93380c
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-01-22 22:16:07 +0800

    Implement echo feature

--- src/main/java/Adam.java
+++ src/main/java/Adam.java
@@ -1,17 +1,35 @@
+import java.util.Scanner;
+
 public class Adam {
     private static void printSeparatingLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 
     private static void outputText(String text) {
-        System.out.println(" " + text);
+        System.out.println("     " + text);
     }
-    
+
     public static void main(String[] args) {
         String CHATBOT_NAME = "Adam";
         printSeparatingLine();
         outputText("Hello! I'm " + CHATBOT_NAME);
         outputText("What can I do for you?");
+        printSeparatingLine();
+
+        Scanner sc = new Scanner(System.in);
+        boolean exited = false;
+
+        while(!exited) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                exited = true;
+            } else {
+                printSeparatingLine();
+                outputText(userInput);
+                printSeparatingLine();
+            }
+        }
+
         printSeparatingLine();
         outputText("Bye. Hope to see you again soon!");
         printSeparatingLine();
