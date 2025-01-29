1807424473c3f6899a14624ac23a926c41ed7599
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-01-29 23:46:47 +0800

    Update error message

--- src/main/java/adam/exceptions/InvalidCommand.java
+++ src/main/java/adam/exceptions/InvalidCommand.java
@@ -2,7 +2,8 @@ package adam.exceptions;
 
 public class InvalidCommand extends AdamException {
     private static String defaultErrorMsg =
-            "Command not recognised! Valid commands are: list, todo, deadline, event, bye";
+            "Command not recognised! Valid commands are: list, listOn, mark, unmark, " +
+            "todo, deadline, event, bye";
     
     public InvalidCommand() {
         super(InvalidCommand.defaultErrorMsg);
