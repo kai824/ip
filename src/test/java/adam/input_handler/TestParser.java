6efdbfc9d836b17c8cd9414443c9fe2ef95a025d
Tan Yi Kai <yikai.tan.sg@gmail.com>
2025-01-29 23:06:23 +0800

    Add JUnit tests for Parser and TaskList

---
+++ src/test/java/adam/input_handler/TestParser.java
@@ -0,0 +1,59 @@
+package adam.input_handler;
+
+import adam.exceptions.AdamException;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestParser {
+    @Test
+    public void testParseInputDate() {
+        try {
+            assertEquals("2021-12-31", Parser.parseInputDate("31-12-2021").toString());
+        } catch (Exception e) {
+            assertEquals(1, 0);
+        }
+    }
+
+    @Test
+    public void testToOutputDate() {
+        try {
+            assertEquals("31 Dec 2021", Parser.toOutputDate(Parser.parseInputDate("31-12-2021")));
+        } catch (Exception e) {
+            assertEquals(1, 0);
+        }
+    }
+
+    @Test
+    public void testToLogDate() {
+        try {
+            assertEquals("31-12-2021", Parser.toLogDate(Parser.parseInputDate("31-12-2021")));
+        } catch (Exception e) {
+            assertEquals(1, 0);
+        }
+    }
+
+    @Test
+    public void testParseInput() {
+        try {
+            assertEquals(AddCommand.class, Parser.parseInput("todo test").getClass());
+            assertEquals(AddCommand.class, Parser.parseInput("deadline test /by 31-12-2021").getClass());
+            assertEquals(AddCommand.class, Parser.parseInput("event test /from 15-12-2021 /to 25-12-2021").getClass());
+            assertEquals(ListCommand.class, Parser.parseInput("list").getClass());
+            assertEquals(ListOnCommand.class, Parser.parseInput("listOn 31-12-2021").getClass());
+            assertEquals(DoneCommand.class, Parser.parseInput("mark 1").getClass());
+            assertEquals(UnmarkCommand.class, Parser.parseInput("unmark 1").getClass());
+            assertEquals(DeleteCommand.class, Parser.parseInput("delete 1").getClass());
+            assertEquals(ByeCommand.class, Parser.parseInput("bye").getClass());
+        } catch (AdamException e) {
+            assertEquals(1, 0);
+        }
+
+        try {
+            Parser.parseInput("invalid command");
+            assertEquals(1, 0);
+        } catch (AdamException e) {
+            assertEquals(1, 1);
+        }
+    }
+}
